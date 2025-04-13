2a9d5497ceb8245e0ccca32b9aa57ae14f7a61b8
RMxitaryan <romanmxitaryan12@gmail.com>
2025-04-13 15:59:44 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { createUseStyles } from "react-jss";
+import sound from "./sounds/127.mp3";
+
+const useStyle = createUseStyles({
+  main: {
+    height: "100dvh",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  cont: {
+    display: "flex",
+    alignItems: "center",
+  },
+  plus: {
+    width: 150,
+    height: 150,
+    cursor: "pointer",
+    "&:hover": {
+      transform: "scale(1.2)",
+      transition: "opacity .3s ease 0s,background-color .3s ease 0s",
+    },
+  },
+  minus: {
+    width: 133,
+    height: 133,
+    cursor: "pointer",
+    "&:hover": {
+      transform: "scale(0.8)",
+      transition: "opacity .3s ease 0s,background-color .3s ease 0s",
+    },
+  },
+});
 
 function App() {
+  const classes = useStyle();
+  const [number, setNumber] = useState(125);
+
+  function play() {
+    new Audio(sound).play();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={classes.main}>
+        <div className={classes.cont}>
+          <img
+            src="/img/plus.png"
+            className={classes.plus}
+            onClick={() => {
+              if (number === 126) {
+                play();
+              }
+
+              setNumber(number + 1);
+            }}
+          />
+          <div style={{ fontSize: 300 }}>{number}</div>
+          <img
+            src="/img/minus.png"
+            className={classes.minus}
+            onClick={() => {
+              if (number === 128) {
+                play();
+              }
+              setNumber(number - 1);
+            }}
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
